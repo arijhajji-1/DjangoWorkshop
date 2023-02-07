6fa41b6e2c08598b550fbef7a1891f0b95c2f98d
arijhajji <61475226+arijhajji-1@users.noreply.github.com>
2023-02-07 10:15:04 +0100

    another commit

--- events/admin.py
+++ events/admin.py
@@ -5,6 +5,34 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'state')
     ordering = ('title',)
     list_filter = ('state',)
+    search_fields = ['title', 'category']
+    list_per_page = 5
+    #fields = ('organizers',
+             # 'state',
+              #('title','description'),
+             # 'eventImage',
+              #  ('category', 'nbrParticipants'),
+              #  'eventDate',
+              #  ('created_at', 'updated_at')
+             # )
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        (
+            'STATE', {
+                'classes' : ('collapse',),
+                'fields': ('state','created_at', 'updated_at')
+            }
+
+        ),
+        (
+            'ABOUT', {
+                'fields': ('title', 'description', 'eventImage', 'category', 'nbrParticipants',
+                           'eventDate')
+            }
+        )
+
+
+        )
 
 # Register your models here.
 admin.site.register(Event, EventAdmin)
