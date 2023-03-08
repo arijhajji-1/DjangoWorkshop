717f51d9bfb0a72a60558f88e134f62777f94c9f
arijhajji <61475226+arijhajji-1@users.noreply.github.com>
2023-03-08 11:49:56 +0100

    login /logout

--- events/urls.py
+++ events/urls.py
@@ -7,13 +7,6 @@ urlpatterns = [
     path('list/', listEvents, name="events_list"),
     path('add/', addEvent, name="events_add"),
     path('details/<int:id>', detailsEvent, name="events_details"),
-    path('Participate/<int:id>', participateEvent, name="events_Participate"),
-    path('update/<int:id>', updateEvent, name="events_update"),
-    path('updateView/<int:pk>', EventUpdateView.as_view(), name="events_updateView"),
-    path('deleteView/<int:pk>', deleteEvent.as_view(), name="events_delete"),
-    path('delete/<int:id>', deleteEventFn, name="events_delete"),
-    path('CancelEvent/<int:id>', cancelEvent, name="events_cancel"),
-
     path('listEvents/', EventsList.as_view(), name="events_listC"),
     path('eventsDetails/<int:pk>', EventsDetails.as_view(), name="events_DetailsC"),
 ]
